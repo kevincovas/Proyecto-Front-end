82169c6f2c555d1f163fadca3720893e45465ecd
Kevin Covas <kevin.covas.91@gmail.com>
2021-06-26 17:10:35 +0200

    Phase 2: components with Hooks without style

--- src/App.js
+++ src/App.js
@@ -1,81 +1,25 @@
 import React from 'react';
-//import axios from 'axios';
+
+
 import LigaEspañola from './components/LigaEspañola';
-import LigaInglesa from './components/LigaInglesa';
 import LigaItaliana from './components/LigaItaliana';
 import LigaFrancesa from './components/LigaFrancesa';
 
-export default class App extends React.Component{
 
-  constructor(props){
-    super(props);
-    this.seleccionLiga = this.seleccionLiga.bind(this);
+export default function App() {  
 
-    this.state = {
-      ligaEspañola:[],
-      premierLeague: [],
-      serieA: [],
-      league1: [],
-      checked: false,
-      ligaSeleccionada: "",
-    }
-    
-  }
-
-  seleccionLiga(event){
-    this.setState({ligaSeleccionada: event.target.value});
-    console.log(this.state.ligaSeleccionada);
-    //this.setState({checked: true});
-  }
-
-  render() {
     return (
       <>
-      {/* <form>
-        <label>
-          Elige una liga:
-          
-        </label>
-
-        <br></br>
-        <br></br>
-
-        <label>
-          <input type="radio" 
-                 value="LigaEspañola"
-                 checked={this.state.checked}
-                 onChange={this.seleccionLiga}
-          />
-          Liga Española
-        </label>
-        <br></br>
-        <label>
-          <input type="radio" value="LigaInglesa"/>
-          Liga Inglesa
-        </label>
-        <br></br>
-        <label>
-          <input type="radio" value="LigaItaliana"/>
-          Liga Italiana
-        </label>
-        <br></br>
-        <label>
-          <input type="radio" value="LigaFrancesa"/>
-          Liga Francesa
-        </label>
-      </form> */}
-
         <h1>Máximos goleadores temporada 2020-2021:</h1>
         <p>A continuación vamos a mostrar los 10 máximos goleadores de las mejores ligas del mundo.</p>
         
         <LigaEspañola />
-        <LigaInglesa />
-        <LigaItaliana />
         <LigaFrancesa />
+        <LigaItaliana />
+      
       </>
     );
-  }
-
+  
 }
 
 const tableStyle = {
--- src/components/LigaEspañola.js
+++ src/components/LigaEspañola.js
@@ -1,63 +1,53 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default class LigaEspañola extends React.Component{
+export default function LigaEspañola() {
+  const [data, setData] = useState({ scorers: [] });
 
-    constructor(props){
-        super(props);
-        this.state = {
-          ligaEspañola:[]
-        }
-      }
-    
-      render() {
-        return (
-          <>
-            <h2>Máximos goleadores de la Liga Española:</h2>
-            <table style={tableStyle}>
-              <th style={tableStyle}>
-                <td style={tableStyle}>Futbolista:</td>
-                <td style={tableStyle}>Goles:</td>
-                <td style={tableStyle}>País:</td>
-                <td style={tableStyle}>Equipo</td>
-              </th>
-              {this.state.ligaEspañola.map((scorer) => (
-              <tr style={tableStyle}>
-                <td style={tableStyle} key={scorer.player.id}>{scorer.player.name}</td>
-                <td style={tableStyle}> {scorer.numberOfGoals}</td>
-                <td style={tableStyle}>{ scorer.player.countryOfBirth}</td>
-                <td style={tableStyle} key={scorer.team.id}>{scorer.team.name}</td>
-              </tr>))}
-            </table>
-          </>
-        );
-      }
-    
-    
-      async componentDidMount(){
-        
-        const clientToken = "83959dbd079647ab913cb59890c3cc6f";
-        const headers = {'X-Auth-Token': clientToken};
-        
-        const res = await axios.get("https://api.football-data.org/v2/competitions/", {headers});
-        const ligaEspañola = await axios.get("https://api.football-data.org/v2/competitions/PD/scorers", {headers});
-        
-    
-        //Console logs
-        console.log(ligaEspañola.data);
-        this.setState({ligaEspañola: ligaEspañola.data.scorers});
-        const info = this.state.ligaEspañola;
-        //console.log(info);
-      }
-    
+  useEffect(() => {
+    const fetchData = async () => {
+      const clientToken = "83959dbd079647ab913cb59890c3cc6f";
+      const headers = { "X-Auth-Token": clientToken };
+      const resApi = await axios(
+        "https://api.football-data.org/v2/competitions/PD/scorers",
+        { headers }
+      );
+      setData(resApi.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>Máximos goleadores de la Liga Española:</h2>
+      <table style={tableStyle}>
+        <th style={tableStyle}>
+          <td style={tableStyle}>Futbolista:</td>
+          <td style={tableStyle}>Goles:</td>
+          <td style={tableStyle}>País:</td>
+          <td style={tableStyle}>Equipo</td>
+        </th>
+        {data.scorers.map((scorer) => (
+          <tr style={tableStyle}>
+            <td style={tableStyle} key={scorer.player.id}>
+              {scorer.player.name}
+            </td>
+            <td style={tableStyle}> {scorer.numberOfGoals}</td>
+            <td style={tableStyle}>{scorer.player.countryOfBirth}</td>
+            <td style={tableStyle} key={scorer.team.id}>
+              {scorer.team.name}
+            </td>
+          </tr>
+        ))}
+      </table>
+    </>
+  );
 }
-const tableStyle = {
-      
-      tableLayout: 'fixed',
-      width: '100',
-      borderCollapse: 'collapse',
-      border: 'solid',
-    
-      
-};
 
+const tableStyle = {
+  tableLayout: "fixed",
+  width: "100",
+  borderCollapse: "collapse",
+  border: "solid",
+};
